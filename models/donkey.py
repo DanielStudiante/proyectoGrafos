1f41ebc55343847356dd5fd5284d53ae8cb28296
karen050521 <tamayodahiana51@gmail.com>
2025-11-07 17:07:28 -0500

    continuación de la implementación del backend

--- models/donkey.py
+++ models/donkey.py
@@ -1,11 +1,11 @@
+from time import sleep
 
 class Donkey:
-    def __init__(self, name, age, donkey_energy, donkey_speed, life_time, grass_in_basement):
+    def __init__(self, name, age, max_age, donkey_energy, grass_in_basement):
         self.name:str = name
-        self.age:int = age
+        self.age:float = age
+        self.max_age:float = max_age
         self.donkey_energy:float = donkey_energy
-        self.donkey_speed:float = donkey_speed
-        self.life_time:float = life_time
         self.alive:bool = True
         self.grass_in_basement = grass_in_basement
         self.damage_stars:float = self.calculate_damage_per_trip()
@@ -15,18 +15,18 @@ class Donkey:
     def calculate_damage_per_trip(self, is_constellation:bool = False):
         damage:float = 0
         match self.age:
-            case age if 0 <= age < 5:
-                damage = 0.01
-                if is_constellation: damage =0.02
-            case v if 5 <= v < 10:
-                damage = 0.02
-                if is_constellation: damage =0.03
-            case v if 10 <= v < 15:
-                damage = 0.03
-                if is_constellation: damage =0.04
+            case age if 0 <= age < 891:
+                damage = 0.05
+                if is_constellation: damage = 0.08
+            case v if 891 <= v < 1783:
+                damage = 0.10
+                if is_constellation: damage = 0.15
+            case v if 1783 <= v < 2675:
+                damage = 0.15
+                if is_constellation: damage = 0.20
             case _:
-                damage = 0.04
-                if is_constellation: damage =0.05
+                damage = 0.20
+                if is_constellation: damage = 0.25
         return damage
 
     def calculate_donkey_health(self):
@@ -59,30 +59,72 @@ class Donkey:
             case _:
                 return 1
 
-    def eat_grass(self, ):
+    def eat_grass(self, grass_profit:float = 1.0):
         if self.grass_in_basement <= 0:
-            return "No hay hierba en el sótano para que el burro coma."
+            print("No hay hierba en el sótano para que el burro coma.")
+            return False
+        if self.donkey_energy >= 100:
+            print("El burro ya tiene energía completa y no necesita comer.")
+            return False
+        self.donkey_energy += 1 * grass_profit
+        self.grass_in_basement -= 1
+        self.health = self.calculate_donkey_health()
+        return True
+
 
     def dead(self):
         self.alive = False
         self.damage_stars = 0
 
-    def journey_through_stars(self, distance:float, time_to_eat:int = 0):
+    def stay_of_star(self, time_to_eat_kg:float = 0, time_of_stance:float = 0):
+        if not self.alive:
+            return "El burro está muerto y no puede explorar."
+        
+        if self.donkey_energy < 50:
+            time_investigate, time_to_eat  = time_of_stance * 0.5
+            for _ in range(int(time_to_eat)):
+                if self.eat_grass(self.calculate_grass_profit()):
+                    sleep(time_to_eat_kg)
+                    print("El burro ha comido 1 kg de hierba.")
+                else:
+                    print("El burro no pudo comer más hierba.")
+                    break
+
+        else:
+            time_investigate = time_of_stance
+            
+            # implementar logica de investigación
+
+    def trip(self, distance:float, time_to_eat_kg:float = 0, time_of_stance:float = 0, is_star:bool = True):
         if not self.alive:
             return "El burro está muerto y no puede viajar."
 
-        self.life_time -= distance
-        self.donkey_energy *= self.damage_stars
-        self.health = self.calculate_donkey_health()
+        self.age += distance
+        if is_star:
+            self.donkey_energy *= self.damage_stars
+        else:
+            self.donkey_energy *= self.damage_constellations
 
-        if self.life_time <= 0 or self.donkey_energy <= 0:
+        if self.age >= self.max_age or self.donkey_energy <= 0:
             self.dead()
             return "El burro ha muerto durante el viaje."
+        
+        self.health = self.calculate_donkey_health()
+        if is_star:
+            self.stay_of_star(time_to_eat_kg, time_of_stance)
 
-        time_of_stance = 2 * time_to_eat
+    def hyper_star(self, distance:float):
+        if not self.alive:
+            return "El burro está muerto y no puede viajar."
 
-        if self.donkey_energy < 50:
-            self.eat_grass()
-            time_investigate = time_to_eat
-        else:
-            time_investigate = time_of_stance
+        self.age += distance
+
+        if self.age >= self.max_age or self.donkey_energy <= 0:
+            self.dead()
+            return "El burro ha muerto durante el viaje."
+        
+        self.donkey_energy *= 1.5
+        self.grass_in_basement *= 2
+
+        
+        self.health = self.calculate_donkey_health()
